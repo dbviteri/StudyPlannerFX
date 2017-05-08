dcf4578c17c9e5e24a73a396ec107976e7cfa220
DidacV <diego.viteri.18@gmail.com>
2017-05-08 19:22:05 +0100

    Added dashboard support

--- src/View/SemesterView.java
+++ src/View/SemesterView.java
@@ -1,20 +1,13 @@
 package View;
 
-import Controller.AssessmentController;
-import Controller.ModuleController;
+import Controller.DatabaseHandler;
 import Controller.SemesterController;
-import Model.Assessment;
-import Model.Module;
-import Model.SemesterProfile;
 import Model.User;
 import Utils.ControlledScene;
 import Utils.StageHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.layout.VBox;
-
-import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by Didac on 07/05/2017.
@@ -29,6 +22,7 @@ public class SemesterView extends SemesterController implements ControlledScene{
     private VBox semesterView;
 
     // Inherits the FXML from DashboardView, needs to have Controller as suffix.
+    // No need to assign it, since FXMLLoader assigns it when SemesterView is loaded.
     @FXML
     private DashBoardView dashboardController;
 
@@ -49,11 +43,7 @@ public class SemesterView extends SemesterController implements ControlledScene{
         //semesterLabel.setText("test");
         if (dbhandler.getUserSession() == null) return;
 
-        User user = dbhandler.getUserSession();
-        SemesterProfile semesterProfile = SemesterController.find(user.getId());
-        ArrayList<Module> modules = ModuleController.findAll(semesterProfile.getSemesterId());
-        ArrayList<Assessment> assessments = AssessmentController.findAll(modules.get(0).getCode());
-
+        User user = DatabaseHandler.getInstance().getUserSession();
         // DECORATE STAGE ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
         userMenu.setText(user.getFirstname());
@@ -69,24 +59,7 @@ public class SemesterView extends SemesterController implements ControlledScene{
 
         // POPULATE DASHBOARD TAB PANE •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
-        // Decide what pane should the assignments go based on deadline
-        Date date = new Date();
-        for (Assessment assessment : assessments) {
-
-            // If compareTo is more than 0, date is after the deadline
-            // Should go to missed deadlines
-
-            // If compareTo is less than 0, date is before the deadline
-            // Should go to upcoming deadlines
-            if (date.compareTo(assessment.getDeadLine()) < 0) {
-                dashboardController.upcomingDeadlinesLbl.setText(assessment.getTitle());
-            } else if (date.compareTo(assessment.getDeadLine()) > 0) {
-                dashboardController.missedDeadlinesLbl.setText(assessment.getTitle());
-            }
-
-            System.out.println(date.compareTo(assessment.getDeadLine()));
-        }
-
+        dashboardController.load();
         //dashboardController.assignments.setText(assessments.get(0).getTitle());
 
         System.out.println(user.getEmail());
